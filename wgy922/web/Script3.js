fec108a5a3781004efb437f1231eb462c5728ff7
w-Lzy <3441131153@qq.com>
2024-12-05 12:52:05 +0800

    Add files via upload

---
+++ wgy922/web/Script3.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var curcontent = document.getElementById('section3');
+    curcontent.style.maxHeight = curcontent.scrollHeight + 'px';
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const buttons = document.querySelectorAll('.toggle-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const targetId = button.dataset.target;
+            const target = document.getElementById(targetId);
+            if (target.style.maxHeight === '0px' || target.style.maxHeight === '') {
+                target.style.maxHeight = target.scrollHeight + "px";
+            } else {
+                target.style.maxHeight = '0px';
+            }
+        });
+    });
+});
